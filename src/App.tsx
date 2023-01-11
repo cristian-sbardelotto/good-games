78dc10f8ccaa2ba7a882ff0b0c924ea2af4d9055
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-01-10 23:09:47 -0300

    feat(hero): implementing the initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SideBar from './components/SideBar';
+import Hero from './components/Hero';
 
 import './main.scss';
 
 const App = () => {
   return <div className='App'>
     <SideBar />
+    <Hero />
   </div>;
 };
 
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import './index.scss';
+
+const Hero = () => {
+  return (
+    <main>
+      <div className='navbar'>
+        <input type='text' placeholder='Procurar' />
+
+        <span>Will Smith</span>
+        <img src='' alt='' />
+
+        <img src='' alt='' />
+        <img src='' alt='' />
+      </div>
+
+      <article>
+        <div>
+          <img src='' alt='' />
+
+          <h5></h5>
+
+          <span></span>
+
+          <span></span>
+
+          <hr />
+
+          <p></p>
+
+          <button></button>
+          <button></button>
+        </div>
+
+        <div>
+          <img src='' alt='' />
+          <div>
+            <img src='' alt='' />
+            <p></p>
+          </div>
+
+          <div>
+            <img src='' alt='' />
+            <p></p>
+          </div>
+        </div>
+      </article>
+    </main>
+  );
+};
+
+export default Hero;
