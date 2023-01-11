6dc692b67b83f57bd215b993c64d915da4b64a84
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-01-11 19:46:21 -0300

    feat(navbar): finished the component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 
+import iconSearch from '../../assets/btn-search.svg';
+import iconUser from '../../assets/user.svg';
+import iconNotifications from '../../assets/icon-notifications.svg';
+import iconPaperBag from '../../assets/icon-paper-bag.svg';
+
 import './index.scss';
 
 const Hero = () => {
   return (
     <main>
-      <div className='navbar'>
-        <input type='text' placeholder='Procurar' />
+      <nav>
+        <div className='input-group'>
+          <input type='text' placeholder='Procurar' />
+          <img src={iconSearch} alt='Search input icon' />
+        </div>
 
-        <span>Will Smith</span>
-        <img src='' alt='' />
+        <div className='user-menu'>
+          <span>Will Smith</span>
+          <img src={iconUser} alt='User' />
 
-        <img src='' alt='' />
-        <img src='' alt='' />
-      </div>
+          <img src={iconNotifications} alt='Notifications' />
+          <img src={iconPaperBag} alt='Paper Bag' />
+        </div>
+      </nav>
 
       <article>
         <div>
