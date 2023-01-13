05b5c7a962a479a2df754c88a9a18b34971fe6a1
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-01-12 22:28:47 -0300

    feat(button group): finished the section

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -12,6 +12,18 @@ import './index.scss';
 const Products = () => {
   return (
     <div className='products-section'>
+      <div className='products-menu'>
+        <span>Nossos Produtos</span>
+
+        <div className='button-group'>
+          <button className='active-btn'>TOP</button>
+          <button>Popular</button>
+          <button>Mais Vendidos</button>
+        </div>
+
+        <span>Categoria</span>
+      </div>
+
       <div className='first-list'>
         <Card
           image={vrGlass}
