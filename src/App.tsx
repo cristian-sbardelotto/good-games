290d4f01c69fa67b80ccc9af6f94b832bbda55f8
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-01-12 22:00:14 -0300

    feat(product cards section): finished the component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 import SideBar from './components/SideBar';
 import Hero from './components/Hero';
-import Card from './components/Card';
-
-import vrGlass from './assets/vr-glass.svg';
+import Products from './components/Products/index';
 
 import './main.scss';
 
@@ -12,7 +10,7 @@ const App = () => {
   return <div className='App'>
     <SideBar />
     <Hero />
-    <Card image={vrGlass} title='Óculos Virtual' name='VR Glasses' price={32} />
+    <Products />
   </div>;
 };
 
---
+++ src/components/Products/index.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import Card from '../Card/index';
+import vrGlass from '../../assets/vr-glass.svg';
+import keyboard from '../../assets/keyboard.svg';
+import soundBox from '../../assets/sound-box.svg';
+import monitor from '../../assets/monitor.svg';
+import pc from '../../assets/pc.svg';
+
+import './index.scss';
+
+const Products = () => {
+  return (
+    <div className='products-section'>
+      <div className='first-list'>
+        <Card
+          image={vrGlass}
+          title='Óculos Virtual'
+          name='VR Glasses'
+          price={32}
+        />
+
+        <Card
+          image={keyboard}
+          title='Teclado Mecânico'
+          name='Mechanic Keyboard'
+          price={200}
+        />
+
+        <Card
+          image={monitor}
+          title='Monitor 25 Polegadas'
+          name='Monitor'
+          price={400}
+        />
+
+        <Card
+          image={soundBox}
+          title='Caixa de Som JBL'
+          name='JBL Sound Box'
+          price={900}
+        />
+
+        <Card
+          image={pc}
+          title='Computador Gamer'
+          name='Gamer Computer'
+          price={900}
+        />
+      </div>
+
+      <div className='second-list'>
+        <Card
+          image={vrGlass}
+          title='Óculos Virtual'
+          name='VR Glasses'
+          price={32}
+        />
+
+        <Card
+          image={keyboard}
+          title='Teclado Mecânico'
+          name='Mechanic Keyboard'
+          price={200}
+        />
+
+        <Card
+          image={monitor}
+          title='Monitor 25 Polegadas'
+          name='Monitor'
+          price={400}
+        />
+
+        <Card
+          image={soundBox}
+          title='Caixa de Som JBL'
+          name='JBL Sound Box'
+          price={900}
+        />
+
+        <Card
+          image={pc}
+          title='Computador Gamer'
+          name='Gamer Computer'
+          price={900}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Products;
